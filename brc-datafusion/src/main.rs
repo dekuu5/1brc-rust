28d0e50ab66f9f607fba100475d7c151198f3bed
Ahmed hossam <ahmed.hossambahig@gmail.com>
2026-01-06 14:01:03 +0200

    v1 of the datafusion solution done

--- brc-datafusion/src/main.rs
+++ brc-datafusion/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::error::Result;
+use datafusion::prelude::*;
+
+
+fn get_input_file() -> String {
+    let mut args = env::args();
+    args.next();
+    args.next().unwrap_or_else(|| {
+        eprintln!("Usage: <program> <input_file>");
+                std::process::exit(1);
+    })
+}
+
+
+#[tokio::main]
+async fn main(){
+    
+    let path = get_input_file();
+    
+    if let Err(e) = run(path).await {
+            eprintln!("Error: {e}");
+    }
+    
+}
+
+async fn run(path: String) -> Result<()> { 
+    let ctx = SessionContext::new();
+    
+    let schema = Schema::new(vec![
+        Field::new("station", DataType::Utf8, false),
+        Field::new("temperature", DataType::Float64, false),
+    ]);
+    
+    let options = CsvReadOptions::new()
+        .delimiter(b';')
+        .has_header(false)
+        .schema(&schema);
+    ctx.register_csv("measurements", &path, options).await?;
+    
+    let df = ctx.sql(
+        "SELECT station, MIN(temperature) as min_tmp,AVG(temperature) as avg_tmp, MAX(temperature) as max_tmp from measurements GROUP by station ORDER by station"
+    ).await?;
+    df.show().await?;
+    Ok(())
 }
