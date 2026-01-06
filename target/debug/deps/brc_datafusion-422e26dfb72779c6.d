ce8f75c8f376b4b533d5462e0a2cb34323ea5813
Ahmed hossam <ahmed.hossambahig@gmail.com>
2026-01-06 12:45:52 +0200

    init datafusion part

---
+++ target/debug/deps/brc_datafusion-422e26dfb72779c6.d
@@ -0,0 +1,5 @@
+/home/ahmed/coding/personal/1brc/1brc-rust/target/debug/deps/brc_datafusion-422e26dfb72779c6.d: brc-datafusion/src/main.rs
+
+/home/ahmed/coding/personal/1brc/1brc-rust/target/debug/deps/libbrc_datafusion-422e26dfb72779c6.rmeta: brc-datafusion/src/main.rs
+
+brc-datafusion/src/main.rs:
