3c24fc5fd312d075c9a3c61c804e8f2e6a45efd2
Ahmed hossam <ahmed.hossambahig@gmail.com>
2026-01-06 12:47:27 +0200

    Remove target directory from version control

--- target/debug/deps/brc_datafusion-2f1d63a22841f08d.d
+++
@@ -1,5 +0,0 @@
-/home/ahmed/coding/personal/1brc/1brc-rust/target/debug/deps/brc_datafusion-2f1d63a22841f08d.d: brc-datafusion/src/main.rs
-
-/home/ahmed/coding/personal/1brc/1brc-rust/target/debug/deps/libbrc_datafusion-2f1d63a22841f08d.rmeta: brc-datafusion/src/main.rs
-
-brc-datafusion/src/main.rs:
